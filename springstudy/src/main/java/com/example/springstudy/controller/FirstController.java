2f9ca3c296f7155094c3fd9a067c0ad1e7a21929
wongakim-99 <202021000@sangmyung.kr>
2024-08-05 17:16:32 +0900

    #3 - 모델 추가하기
    username이라는 변수를 사용해서 이름 대신 들어갈 변수명을 사용해서 변수값에 따라 결과가 그때그때 다르게 출력됨

--- springstudy/src/main/java/com/example/springstudy/controller/FirstController.java
+++ springstudy/src/main/java/com/example/springstudy/controller/FirstController.java
@@ -1,13 +1,16 @@
 package com.example.springstudy.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller  // 컨트롤러 선언
 public class FirstController {
 
     @GetMapping("/hi")
-    public String niceToMeetYou() {
+    public String niceToMeetYou(Model model) {
+        // model 객체가 "가원" 값을 "username"에 연결해 웹 브라우저로 보냄
+        model.addAttribute("username", "가원");
         return "greetings";  // greetings.mustache 파일 반환
     }
 }
