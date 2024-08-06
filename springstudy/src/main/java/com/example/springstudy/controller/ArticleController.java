10d21c4076dc4cb3c7928287fa801ce45dea9b49
wongakim-99 <202021000@sangmyung.kr>
2024-08-06 17:36:43 +0900

    #4 - 폼 데이터 받기
    DTO 만들고, 입력폼과 DTO 필드 연결해서 폼 데이터를 받음

--- springstudy/src/main/java/com/example/springstudy/controller/ArticleController.java
+++ springstudy/src/main/java/com/example/springstudy/controller/ArticleController.java
@@ -1,12 +1,20 @@
 package com.example.springstudy.controller;
 
+import com.example.springstudy.dto.ArticleForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller  // 컨트롤러 선언
 public class ArticleController {  // URL 요청 접수
     @GetMapping("/articles/new")  // 메서드 생성 및 반환값 작성
     public String newArticleForm() {
         return "articles/new";
+    }
+
+    @PostMapping("/articles/create")
+    public String createArticle(ArticleForm form) {  // 폼 데이터를 DTO로 받기
+        System.out.println(form.toString());  // DTO에 폼 데이터가 잘 담겼는지 확인
+        return "";
     }
 }
