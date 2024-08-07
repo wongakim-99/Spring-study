c8f64c6def9449f4bd7ae33925c1ac13d22682ba
wongakim-99 <202021000@sangmyung.kr>
2024-08-08 00:42:18 +0900

    #5 - 롬복과 리팩터링
    println() 기능 대신에 log 기능을 넣어서 전체적인 리팩터링 수행

--- springstudy/src/main/java/com/example/springstudy/dto/ArticleForm.java
+++ springstudy/src/main/java/com/example/springstudy/dto/ArticleForm.java
@@ -1,24 +1,14 @@
 package com.example.springstudy.dto;
 
 import com.example.springstudy.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 public class ArticleForm {
     private String title;  // 제목을 받을 필드
     private String content;  // 내용을 받을 필드
-
-    public ArticleForm(String content, String title) {
-        this.content = content;
-        this.title = title;
-    }
-
-    // 데이터를 잘 받았는지 확인할 toString() 메서드 추가
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
     public Article toEntity() {
         return new Article(null, title, content);
--- springstudy/src/main/java/com/example/springstudy/entity/Article.java
+++ springstudy/src/main/java/com/example/springstudy/entity/Article.java
@@ -4,7 +4,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 @Entity  // 엔티티 선언
 public class Article {
     @Id  // 엔티티의 대표값 지정
@@ -14,20 +18,4 @@ public class Article {
     private String title;
     @Column  // content 필드 선언, DB테이블의 content 열과 연결됨
     private String content;
-
-    // Article 생성자 추가
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-    // toString 메서드 추가
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
