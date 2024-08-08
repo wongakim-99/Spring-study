ed6b2f9119ee984170cfe7426e44f716ad96b7e5
wongakim-99 <202021000@sangmyung.kr>
2024-08-08 13:06:24 +0900

    #6 - DB에 저장된 데이터 조회
    MVC, JPA, DB의 상호작용을 이해

--- springstudy/src/main/java/com/example/springstudy/controller/ArticleController.java
+++ springstudy/src/main/java/com/example/springstudy/controller/ArticleController.java
@@ -6,7 +6,9 @@ import com.example.springstudy.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
@@ -32,5 +34,19 @@ public class ArticleController {  // URL 요청 접수
         Article saved = articleRepository.save(article);  // article 엔티티를 저장해 saved 객체에 반환
         log.info(saved.toString());  // article 이 DB에 잘 저장되는지 확인 출력
         return "";
+    }
+
+    @GetMapping("/articles/{id}")  // 데이터 조회 요청 접수
+    public String show(@PathVariable Long id, Model model) {
+        log.info("id = " +id);  // id를 잘 받았는지 확인하는 로그 찍기
+
+        // 1. id를 조회해 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);  // orElse 메서드 추가 후 매개변수 null 추가
+
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+
+        // 3. 뷰 페이지 반환하기
+        return "articles/show";
     }
 }
